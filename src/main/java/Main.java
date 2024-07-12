39e9dc5653d4d39d31a70482355026f4c39eee42
Evgeny Nikitin <ev0909@rambler.ru>
2024-07-12 15:55:02 +0300

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -73,31 +74,28 @@ public class Main {
     }
 
 
-    private static List<Employee> parseXML(String xmlFilename) throws ParserConfigurationException, IOException, SAXException {
+    private static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
         List<String> elements = new ArrayList<>();
         List<Employee> list = new ArrayList<>();
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new File(xmlFilename));
+        Document doc = builder.parse(fileName);
         Node root = doc.getDocumentElement();
         NodeList nodeList = root.getChildNodes();
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
-            if (node.getNodeName().equals("employee")) {
-                NodeList nodeList1 = node.getChildNodes();
-                for (int j = 0; j < nodeList1.getLength(); j++) {
-                    Node node_ = nodeList1.item(j);
-                    if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                        elements.add(node_.getTextContent());
-                    }
-                }
-                list.add(new Employee(
-                        Long.parseLong(elements.get(0)),
-                        elements.get(1),
-                        elements.get(2),
-                        elements.get(3),
-                        Integer.parseInt(elements.get(4))));
-                elements.clear();
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element employee = (Element) node;
+
+                long id = Integer.parseInt(employee.getElementsByTagName("id").item(0).getTextContent());
+                String firstName = employee.getElementsByTagName("firstName").item(0).getTextContent();
+                String lastName = employee.getElementsByTagName("lastName").item(0).getTextContent();
+                String country = employee.getElementsByTagName("country").item(0).getTextContent();
+                int age = Integer.parseInt(employee.getElementsByTagName("age").item(0).getTextContent());
+
+
+                Employee empl = new Employee(id, firstName, lastName, country, age);
+                list.add(empl);
             }
         }
         return list;
